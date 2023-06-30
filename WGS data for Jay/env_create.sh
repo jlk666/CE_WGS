c635960b6e2eb352668e1ee1a519b6bd604fb534
jlk666 <zsliu@ucdavis.edu>
2023-06-29 22:41:20 -0700

    first commit

---
+++ WGS data for Jay/env_create.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -login
+#SBATCH -p med2                # use 'med2' partition for medium priority
+#SBATCH -J QC control               # name for job
+#SBATCH -c 16                   # 16 core
+#SBATCH -t 2:00:00             # ask for an day
+#SBATCH --mem=16000             # memory (2000 mb = 16gb)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=zsliu@ucdavis.edu
+
+# initialize conda
+. ~/miniconda3/etc/profile.d/conda.sh
+
+# activate your desired conda environment
+conda activate de_novo_assembly
+
+# fail on weird errors
+set -e
+set -x
+
+### YOUR COMMANDS GO HERE ###
+# for example,
+fastqc *.fastq.gz *.fastq.gz -t 6
+### YOUR COMMANDS GO HERE ###
+
+# Print out values of the current jobs SLURM environment variables
+env | grep SLURM
+
+# Print out final statistics about resource use before job exits
+sscontrol show job ${SLURM_JOB_ID}
+
+sstat --format 'JobID,MaxRSS,AveCPU' -j ${SLURM_JOB_ID}

9a8fb1c70ccf0bf2a895f12ffb8cdd5e9bcbe8aa
jlk666 <zsliu@ucdavis.edu>
2023-07-02 19:21:00 -0700

    annotation done

--- main/Genome_signature/taxnomy.sh
+++ main/Genome_signature/taxnomy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -login
 #SBATCH -p med2                # use 'med2' partition for medium priority
-#SBATCH -J CE_genome_assembly               # name for job
+#SBATCH -J sourmash_CE_genome_identfiy               # name for job
 #SBATCH -c 32                   # 1 core
 #SBATCH -t 24:00:00             # ask for an hour, max
-#SBATCH --mem=16000             # memory (2000 mb = 2gb)
+#SBATCH --mem=32000             # memory (32 mb = 32)
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zsliu@ucdavis.edu
 
@@ -11,7 +11,7 @@
 . ~/miniconda3/etc/profile.d/conda.sh
 
 # activate your desired conda environment
-conda activate VPML
+conda activate smash
 
 # fail on weird errors
 set -e
@@ -20,7 +20,7 @@ set -x
 ### YOUR COMMANDS GO HERE ###
 # for example,
 #snakemake --cores 4 download_genome
-snakemake --cores 32 annotate --use-conda
+sourmash search signature_spades_CE1_S55_L002.sig genbank-2022.03-bacteria-k31.zip
 ### YOUR COMMANDS GO HERE ###
 
 # Print out values of the current jobs SLURM environment variables
